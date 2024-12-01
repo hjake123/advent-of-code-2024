c901583491f4de7e90cc752bc8ef59054650fe8e
hyperlynx <hjake123@gmail.com>
2024-12-01 14:18:58 -0600

    set up day 1

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -5,10 +5,11 @@ pub fn run(input: &str) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs;
 
     #[test]
     fn it_works() {
-        let result = run("apple");
-        assert_eq!(result, 5);
+        let result = run(&fs::read_to_string("./test.txt").unwrap());
+        assert_eq!(result, 11);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 
 fn main() {
-    println!("{}", day00::run(&fs::read_to_string("./test.txt").unwrap()));
+    println!("Day 0: {}", day00::run(&fs::read_to_string("./input/day00.txt").unwrap()));
+    println!("Day 1: {}", day01::run(&fs::read_to_string("./input/day01.txt").unwrap()));
 }
