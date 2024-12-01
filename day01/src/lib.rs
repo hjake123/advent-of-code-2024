2da64a13789c91734adcf194f1417f8781c4079c
hyperlynx <hjake123@gmail.com>
2024-12-01 15:26:36 -0600

    Full day 1 solution

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -1,5 +1,51 @@
-pub fn run(input: &str) -> usize {
-    input.len()
+pub fn run_a(input: &str) -> i32 {
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+
+    for line in input.lines() {
+        let mut halves = line.split_whitespace();
+        left.push(halves.next().unwrap().parse().unwrap());
+        right.push(halves.next().unwrap().parse().unwrap());
+    }
+
+    left.sort_unstable();
+    right.sort_unstable();
+
+    let mut sum = 0;
+    let mut index: usize = 0;
+    while index < left.len() {
+        sum += (left[index] - right[index]).abs();
+        index += 1;
+    }
+    sum
+}
+
+use std::collections::HashMap;
+
+pub fn run_b(input: &str) -> i32 {
+    let mut lefts: Vec<i32> = Vec::new();
+    let mut map: HashMap<i32, i32> = HashMap::new();
+
+    for line in input.lines() {
+        let mut halves = line.split_whitespace();
+        let left = halves.next().unwrap().parse().unwrap();
+        let right = halves.next().unwrap().parse().unwrap();
+        lefts.push(left);
+
+        if map.get(&right).is_none() {
+            map.insert(right, 1);
+        } else {
+            map.insert(right, map.get(&right).unwrap() + 1);
+        }
+    }
+
+    let mut sum: i32 = 0;
+    for left in lefts {
+        sum += map.get(&left).unwrap_or(&0) * left;
+    }  
+
+    sum
+    
 }
 
 #[cfg(test)]
@@ -8,8 +54,14 @@ mod tests {
     use std::fs;
 
     #[test]
-    fn it_works() {
-        let result = run(&fs::read_to_string("./test.txt").unwrap());
+    fn a() {
+        let result = run_a(&fs::read_to_string("./test.txt").unwrap());
         assert_eq!(result, 11);
+    }
+
+    #[test]
+    fn b() {
+        let result = run_b(&fs::read_to_string("./test.txt").unwrap());
+        assert_eq!(result, 31);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ use std::fs;
 
 fn main() {
     println!("Day 0: {}", day00::run(&fs::read_to_string("./input/day00.txt").unwrap()));
-    println!("Day 1: {}", day01::run(&fs::read_to_string("./input/day01.txt").unwrap()));
+    println!("Day 1a: {}", day01::run_a(&fs::read_to_string("./input/day01.txt").unwrap()));
+    println!("Day 1b: {}", day01::run_b(&fs::read_to_string("./input/day01.txt").unwrap()));
 }
