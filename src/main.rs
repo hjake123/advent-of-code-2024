d5eea07fdcda746180f44657edc1a0858050b4fc
hjake123 <hjake123@gmail.com>
2024-12-02 11:54:26 -0600

    Make main able to run only one day or all of them

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,39 @@
 use std::fs;
+use std::env;
+
+const MAX_DAY: i32 = 2;
 
 fn main() {
-    println!("Day 0o: {}", day00::run(&fs::read_to_string("./input/day00.txt").unwrap()));
-    println!("Day 1a: {}", day01::run_a(&fs::read_to_string("./input/day01.txt").unwrap()));
-    println!("Day 1b: {}", day01::run_b(&fs::read_to_string("./input/day01.txt").unwrap()));
-    println!("Day 2a: {}", day02::run_a(&fs::read_to_string("./input/day02.txt").unwrap()));
-    println!("Day 2b: {}", day02::run_b(&fs::read_to_string("./input/day02.txt").unwrap()));
+    let mut daynum: Option<i32> = None;
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        let result = args[1].parse();
+        if result.is_err() {
+            println!("Invalid day number {}", args[1]);
+            return;
+        }
+        daynum = Some(result.unwrap_or(0));
+    }
+    run_day(daynum);
+}
+
+fn run_day(daynum: Option<i32>) {
+    match daynum {
+        Some(1) => {
+            println!("Day 1a: {}", day01::run_a(&fs::read_to_string("./input/day01.txt").unwrap()));
+            println!("Day 1b: {}", day01::run_b(&fs::read_to_string("./input/day01.txt").unwrap()));
+        },
+        Some(2) => {
+            println!("Day 2a: {}", day02::run_a(&fs::read_to_string("./input/day02.txt").unwrap()));
+            println!("Day 2b: {}", day02::run_b(&fs::read_to_string("./input/day02.txt").unwrap()));
+        },
+        Some(n) => {
+            println!("Can't run day number {}", n)
+        }
+        None => {
+            for number in 1..(MAX_DAY + 1) {
+                run_day(Some(number))
+            }
+        }
+    }
 }
