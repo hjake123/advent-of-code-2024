12b6547c261bfd123123018da581d4b160a48da3
hjake123 <hjake123@gmail.com>
2024-12-04 00:18:22 -0600

    borrowing

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -6,9 +6,9 @@ pub fn run_a(input: &str) -> i32 {
     let mut parser = a::Parser::new();
 
     let mut index = 0;
-    let glyphs = &input.chars().collect::<Vec<char>>();
+    let glyphs = input.chars().collect::<Vec<char>>();
     while index < input.len() {
-        parser.run(glyphs, &mut index, &mut sum);
+        parser.run(&glyphs, &mut index, &mut sum);
         index += 1;
     }
     sum
@@ -19,9 +19,9 @@ pub fn run_b(input: &str) -> i32 {
     let mut parser = b::Parser::new();
 
     let mut index = 0;
-    let glyphs = &input.chars().collect::<Vec<char>>();
+    let glyphs = input.chars().collect::<Vec<char>>();
     while index < input.len() {
-        parser.run(glyphs, &mut index, &mut sum);
+        parser.run(&glyphs, &mut index, &mut sum);
         index += 1;
     }
     sum
