30fe3448c80c3a3529a2a5258bd144aa8131701c
hjake123 <hjake123@gmail.com>
2024-12-06 16:28:02 -0600

    Day 6b second attempt. Works for test data, but not for puzzle.

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -26,19 +26,25 @@ impl Direction {
         }
     }
 
-    pub fn offset(&self, x: usize, y: usize) -> (usize, usize) {
+    pub fn offset(&self, x: usize, y: usize) -> Option<(usize, usize)> {
         match *self {
             Self::Up => {
-                (x, y-1)
+                if y == 0 {
+                    return None
+                }
+                Some((x, y-1))
             }
             Self::Right => {
-                (x+1, y)
+                Some((x+1, y))
             }
             Self::Down => {
-                (x, y+1)
+                Some((x, y+1))
             }
             Self::Left => {
-                (x-1, y)
+                if x == 0 {
+                    return None
+                }
+                Some((x-1, y))
             }
         }
     }
@@ -71,7 +77,7 @@ struct Guard {
 
 impl Guard {
     pub fn step(&mut self, layout: &mut Layout) -> bool {
-        let (new_x, new_y) = self.dir.offset(self.x, self.y);
+        let (new_x, new_y) = self.dir.offset(self.x, self.y).expect("Tried to move off screen!");
         if !(layout[self.y][self.x] == 'X') {
             layout[self.y][self.x] = 'X';
             self.visit_count += 1;
@@ -89,34 +95,48 @@ impl Guard {
     }
 
     pub fn part_b_step(&mut self, layout: &mut Layout) -> bool {
-        let (new_x, new_y) = self.dir.offset(self.x, self.y);
+        let (new_x, new_y) = self.dir.offset(self.x, self.y).expect("Tried to move off screen!");
         if !(layout[self.y][self.x] != '.') {
             layout[self.y][self.x] = self.dir.to_char();
         }
-        if new_y >= layout.len() || new_x >= layout[new_y].len() {
+        if !in_bounds(layout, new_x, new_y) {
             return false
         }   
 
-        if layout[new_y][new_x] == '#' {
+        if layout[new_y][new_x] == '#' || layout[new_y][new_x] == '@' {
             self.dir = self.dir.right_from();
+            layout[new_y][new_x] = '@';
         } else {
             // Check the new part B with this logic:
-            // If there is a rightward path to your right,
-            // and no path in front of you,
-            // then the obstacle would put you onto your own past path, making you loop.
-            let right_of_facing = self.dir.right_from().offset(self.x, self.y);
-            if layout[right_of_facing.1][right_of_facing.0] == self.dir.right_from().to_char(){
-                self.visit_count += 1;
-                dbg!(&self);
-                let mut tmp = layout.to_vec();
-                tmp[new_y][new_x] = 'O';
-                print_grid(&tmp);
+            // If there is an already used obstacle on your right
+            // then this is a place where turning right would cause a loop.
+            let right_of_facing = self.dir.right_from();
+            let mut scanner = (self.x, self.y);
+            while in_bounds(layout, scanner.0, scanner.1) {
+                if layout[scanner.1][scanner.0] == '@' {
+                    self.visit_count += 1;
+                    break;
+                }
+                if layout[scanner.1][scanner.0] == '#' {
+                    break;
+                }
+                let possible_scanner = right_of_facing.offset(scanner.0, scanner.1);
+                if possible_scanner.is_none() {
+                    break;
+                }
+                scanner = possible_scanner.unwrap();
             }
+
+            // Normal stop logic resumes.
             self.x = new_x;
             self.y = new_y;
         }
         true
     }
+}
+
+fn in_bounds(layout: &Layout, x: usize, y: usize) -> bool {
+    y < layout.len() && x < layout[y].len()
 }
 
 fn print_grid(vec2d: &Vec<Vec<char>>) {
