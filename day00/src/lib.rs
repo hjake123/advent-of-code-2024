78ce6363991e95a0c7ffe4e1cf8b6b56c5c8a458
hjake123 <hjake123@gmail.com>
2024-12-07 10:12:42 -0600

    Make day00 into a real template

--- day00/src/lib.rs
+++ day00/src/lib.rs
@@ -1,14 +1,25 @@
-pub fn run(input: &str) -> usize {
-    input.len()
+pub fn run_a(input: &str) -> i32 {
+    0
+}
+
+pub fn run_b(input: &str) -> i32 {
+    0
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs;
 
     #[test]
-    fn it_works() {
-        let result = run("apple");
-        assert_eq!(result, 5);
+    fn a() {
+        let result = run_a(&fs::read_to_string("./test.txt").expect("No test file!"));
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn b() {
+        let result = run_b(&fs::read_to_string("./test.txt").expect("No test file!"));
+        assert_eq!(result, 1);
     }
 }
