5006aef46b805a820ff0d0dfabe171ad8cc34172
hjake123 <hjake123@gmail.com>
2024-12-13 11:21:22 -0600

    Complete 13a with memoization

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,10 +1,10 @@
 use core::fmt;
-use std::fmt::{Display, Formatter};
+use std::fmt::Display;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
 pub struct Point {
-    x: usize, 
-    y: usize
+    pub x: usize, 
+    pub y: usize
 }
 
 impl Display for Point {
@@ -13,39 +13,45 @@ impl Display for Point {
     }
 }
 
+impl Point {
+    pub fn offset(&self, x: usize, y: usize) -> Point {
+        Point{x: self.x + x, y: self.y + y}
+    }   
+}
+
 #[derive(Debug, PartialEq)]
 pub struct Grid<T> {
     vec: Vec<Vec<T>>
 }
 
-impl Grid<char> {
-    fn new(input: &str) -> Self {
-        let mut grid: Vec<Vec<char>> = Vec::new();        
-        for line in input.lines() {
-            let mut row = Vec::new();
+// impl Grid<char> {
+//     fn new(input: &str) -> Self {
+//         let mut grid: Vec<Vec<char>> = Vec::new();        
+//         for line in input.lines() {
+//             let mut row = Vec::new();
     
-            for letter in line.chars() {
-                row.push(letter);
-            }
-            grid.push(row);
-        }
-        Grid{vec: grid}
-    }
-}
+//             for letter in line.chars() {
+//                 row.push(letter);
+//             }
+//             grid.push(row);
+//         }
+//         Grid{vec: grid}
+//     }
+// }
 
-impl Grid<i32> {
-    fn new(input: &str) -> Self {
-        let mut grid: Vec<Vec<i32>> = Vec::new();        
-        for line in input.lines() {
-            let mut row = Vec::new();
-            for word in line.split_whitespace() {
-                row.push(word.parse().expect(&("Invalid integer ".to_owned() + word)));
-            }
-            grid.push(row);
-        }
-        Grid{vec: grid}
-    }
-}
+// impl Grid<i32> {
+//     fn new(input: &str) -> Self {
+//         let mut grid: Vec<Vec<i32>> = Vec::new();        
+//         for line in input.lines() {
+//             let mut row = Vec::new();
+//             for word in line.split_whitespace() {
+//                 row.push(word.parse().expect(&("Invalid integer ".to_owned() + word)));
+//             }
+//             grid.push(row);
+//         }
+//         Grid{vec: grid}
+//     }
+// }
 
 #[cfg(test)]
 mod tests {}
--- day13/src/lib.rs
+++ day13/src/lib.rs
@@ -1,15 +1,142 @@
-pub fn run_a(input: &str) -> i32 {
-    input.len().try_into().unwrap()
+use std::{cmp::Ordering, collections::HashMap, str::Lines};
+
+use common::Point;
+
+#[derive(Debug)]
+struct Machine {
+    a_move: Point,
+    b_move: Point, 
+    prize: Point
 }
 
-pub fn run_b(input: &str) -> i32 {
-    input.len().try_into().unwrap()
+fn read_one_machine(line_source: &mut Lines<'_>) -> Option<Machine> {
+    let a_line = line_source.next();
+    let b_line = line_source.next();
+    let p_line = line_source.next();
+
+    if a_line.is_none() || b_line.is_none() || p_line.is_none() {
+        return None
+    }
+
+    let a_line = a_line.unwrap().replace(|c: char| !c.is_ascii_digit() && !c.is_whitespace(), "");
+    let b_line = b_line.unwrap().replace(|c: char| !c.is_ascii_digit() && !c.is_whitespace(), "");
+    let p_line = p_line.unwrap().replace(|c: char| !c.is_ascii_digit() && !c.is_whitespace(), "");
+
+    let mut a_line = a_line.split_whitespace().map(|str| str.parse::<usize>().unwrap());
+    let mut b_line = b_line.split_whitespace().map(|str| str.parse::<usize>().unwrap());
+    let mut p_line = p_line.split_whitespace().map(|str| str.parse::<usize>().unwrap());
+
+    Some(Machine {
+        a_move: Point{x: a_line.next().unwrap(), y: a_line.next().unwrap()},
+        b_move: Point{x: b_line.next().unwrap(), y: b_line.next().unwrap()},
+        prize: Point{x: p_line.next().unwrap(), y: p_line.next().unwrap()}
+    })
+}
+
+/*
+Seek the solution using backtracking and memoization.
+If you always miss the solution, returns None.
+If you don't, returns the number of tokens used to reach a solution.
+*/
+fn hunt_solution(machine: &Machine, claw: Point, tokens_spent: usize, a_presses: usize, b_presses: usize, cache: &mut HashMap<(Point, usize, usize), Option<usize>>, has_max_depth: bool) -> Option<usize> {
+    if has_max_depth && (a_presses > 100 || b_presses > 100) {
+        return None
+    }
+    if cache.contains_key(&(claw, a_presses, b_presses)) {
+        return *cache.get(&(claw, a_presses, b_presses)).unwrap();
+    }
+    if claw == machine.prize {
+        cache.insert((claw, a_presses, b_presses), Some(tokens_spent));
+        return Some(tokens_spent)
+    }
+
+    if claw.x > machine.prize.x || claw.y > machine.prize.y {
+        cache.insert((claw, a_presses, b_presses), None);
+        return None
+    }
+
+    let after_pressing_a = hunt_solution(machine, claw.offset(machine.a_move.x, machine.a_move.y), tokens_spent + 3, a_presses + 1, b_presses, cache, has_max_depth);
+    let after_pressing_b = hunt_solution(machine, claw.offset(machine.b_move.x, machine.b_move.y), tokens_spent + 1, a_presses, b_presses + 1, cache, has_max_depth);
+
+    if after_pressing_a.is_none() {
+        if after_pressing_b.is_none() {
+            cache.insert((claw, a_presses, b_presses), None);
+            return None
+        }
+        cache.insert((claw, a_presses, b_presses), after_pressing_b);
+        return after_pressing_b
+    }
+    if after_pressing_b.is_none() {
+        cache.insert((claw, a_presses, b_presses), after_pressing_a);
+        return after_pressing_a
+    }
+
+    match after_pressing_a.unwrap().cmp(&after_pressing_b.unwrap()) {
+        Ordering::Greater => {
+            cache.insert((claw, a_presses, b_presses), after_pressing_a);
+            return after_pressing_a
+        }
+        Ordering::Less => {
+            cache.insert((claw, a_presses, b_presses), after_pressing_b);
+            return after_pressing_b
+        }
+        Ordering::Equal => {
+            // Doesn't matter which
+            cache.insert((claw, a_presses, b_presses), after_pressing_a);
+            return after_pressing_a
+        }
+    }
+
+}
+
+pub fn run_a(input: &str) -> usize {
+    let mut sum = 0;
+    let mut lines = input.lines();
+    loop {
+        let machine = read_one_machine(&mut lines);
+        let _ = lines.next();
+        if machine.is_none(){
+            break;
+        }
+        let machine = machine.unwrap();
+        let solution = hunt_solution(&machine, Point{x: 0, y: 0}, 0, 0, 0, &mut HashMap::new(), true);
+        if solution.is_some() {
+            sum += solution.unwrap();
+        }
+    }
+    sum
+}
+
+pub fn run_b(input: &str) -> usize {
+    let mut sum = 0;
+    let mut lines = input.lines();
+    loop {
+        let machine = read_one_machine(&mut lines);
+        let _ = lines.next();
+        if machine.is_none(){
+            break;
+        }
+        let mut machine = machine.unwrap();
+        machine.prize.x += 10000000000000;
+        machine.prize.y += 10000000000000;
+        let solution = hunt_solution(&machine, Point{x: 0, y: 0}, 0, 0, 0, &mut HashMap::new(), false);
+        if solution.is_some() {
+            sum += solution.unwrap();
+        }
+    }
+    sum
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::fs;
+
+    #[test]
+    fn a_single() {
+        let result = run_a(&fs::read_to_string("./single.txt").expect("No test file!"));
+        assert_eq!(result, 280);
+    }
 
     #[test]
     fn a() {
