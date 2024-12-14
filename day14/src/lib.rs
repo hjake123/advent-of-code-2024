689f4cac6f8848f7e2f304693a8e4c8c73a9e5fe
hjake123 <hjake123@gmail.com>
2024-12-14 11:48:36 -0600

    Day 14 parsing

--- day14/src/lib.rs
+++ day14/src/lib.rs
@@ -1,9 +1,32 @@
+use common::Point;
+
+#[derive(Debug)]
+struct Robot {
+    position: Point<i32>,
+    velocity: Point<i32>
+}
+
+impl Robot {
+    fn from_line(line: &str) -> Self {
+        let chunks: Vec<i32> = line.replace(|ch: char| !ch.is_ascii_digit() && ch != '-', " ").split_whitespace().map(|chunk| chunk.parse::<i32>().unwrap()).collect();
+        Robot { 
+            position: Point { x: chunks[0], y: chunks[1] }, 
+            velocity: Point { x: chunks[2], y: chunks[3] }
+        }
+    }
+}
+
 pub fn run_a(input: &str) -> i32 {
-    input.len().try_into().unwrap()
+    let mut robots: Vec<Robot> = Vec::new();
+    for line in input.lines() {
+        robots.push(Robot::from_line(line));
+    }
+    dbg!(robots);
+    0
 }
 
 pub fn run_b(input: &str) -> i32 {
-    input.len().try_into().unwrap()
+    0
 }
 
 #[cfg(test)]
@@ -14,7 +37,7 @@ mod tests {
     #[test]
     fn a() {
         let result = run_a(&fs::read_to_string("./test.txt").expect("No test file!"));
-        assert_eq!(result, 1);
+        assert_eq!(result, 12);
     }
 
     #[test]
