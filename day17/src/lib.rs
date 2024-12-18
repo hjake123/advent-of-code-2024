5327389eacd112e2d9d455058be01f3b754417c2
hjake123 <hjake123@gmail.com>
2024-12-17 21:20:58 -0600

    DAY 17 COMPLETE

--- day17/src/lib.rs
+++ day17/src/lib.rs
@@ -9,11 +9,17 @@ pub fn run_a(input: &str) -> String {
     machine.output.iter().map(|num| num.to_string()).collect::<Vec<String>>().join(",")
 }
 
-fn get_output_digit(partial_a: u64) -> i8 {
-    ((partial_a % 8) ^ 3 ^ 5 ^ (partial_a.checked_shr((partial_a ^ 3).try_into().unwrap()).unwrap_or(0)) % 8).try_into().unwrap()
+fn get_output_digit(a: u64) -> i8 {
+    let b = a % 8;
+    let b = b ^ 3;
+    let c = a >> b;
+    // let a = a >> 3;
+    let b = b ^ 5;
+    let b = b ^ c;
+    (b % 8).try_into().unwrap()
 }
 
-fn consume_output(mut outputs: VecDeque<i8>, prior_a: u32) -> Option<u32> {
+fn consume_output(mut outputs: VecDeque<i8>, prior_a: u64) -> Option<u64> {
     if outputs.is_empty(){
         return Some(prior_a);
     }
@@ -30,13 +36,11 @@ fn consume_output(mut outputs: VecDeque<i8>, prior_a: u32) -> Option<u32> {
     None
 }
 
-pub fn run_b(input: &str) -> u32 {
+pub fn run_b(input: &str) -> u64 {
     // Not a general solution!
-    // Each number of output = A XOR 3 XOR 5 XOR (A << (A XOR 3)) % 8 for my puzzle input
-    // Due to A << (up to 7), each digit of output takes into account 10 bits of A.
     let machine = Machine::parse(input);
     let outputs: VecDeque<i8> = VecDeque::from(machine.program.into_iter().rev().collect::<Vec<i8>>());
-    let mut results: Vec<u32> = Vec::new();
+    let mut results: Vec<u64> = Vec::new();
     for a_starter in 0..128 {
         let res = consume_output(outputs.clone(), a_starter);
         if res.is_some(){
@@ -65,9 +69,31 @@ mod tests {
     }
 
     #[test]
+    fn b_get_digit(){
+        let mut a = 63687530;
+        assert_eq!(get_output_digit(a), 1);
+        a >>= 3;
+        assert_eq!(get_output_digit(a), 6);
+        a >>= 3;
+        assert_eq!(get_output_digit(a), 7);
+        a >>= 3;
+        assert_eq!(get_output_digit(a), 4);
+        a >>= 3;
+        assert_eq!(get_output_digit(a), 3);
+        a >>= 3;
+        assert_eq!(get_output_digit(a), 0);
+        a >>= 3;
+        assert_eq!(get_output_digit(a), 5);
+        a >>= 3;
+        assert_eq!(get_output_digit(a), 0);
+        a >>= 3;
+        assert_eq!(get_output_digit(a), 6);
+    }
+
+    #[test]
     fn puzzle_input_sc() {
         let mut machine = Machine::parse(&fs::read_to_string("./pisc.txt").expect("No test file!"));
-        machine.a = run_b(&fs::read_to_string("./pisc.txt").expect("No test file!")).into();
+        machine.a = run_b(&fs::read_to_string("./pisc.txt").expect("No test file!")).try_into().unwrap();
         while machine.run() {}
         let result = machine.output.iter().map(|num| num.to_string()).collect::<Vec<String>>().join(",");
         assert_eq!(result, "2,4,1,3,7,5,0,3,1,5,4,1,5,5,3,0");
